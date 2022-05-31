065950967d266b8c16bedf6827f73681fc3b3d55
Artem <artemaistrike@gmail.com>
2022-05-31 10:35:38 +0300

    add works img

--- js/projects.js
+++ js/projects.js
@@ -3,13 +3,19 @@ const alert = document.querySelector(".works-alert");
 
 /// Запрос репозиториев с GitHub API и добавление их на страницу
 
-function addWork(name, url) {
+function addWork(name, url, type) {
     const work = document.createElement("div");
     const workLink = document.createElement("a");
+    const img = document.createElement("img");
+
+    img.alt = "type";
+    img.src = `../img/code-languages/${type.toLowerCase()}.png`;
     work.classList.add("work");
     workLink.href = url;
-    workLink.textContent = name;
+    workLink.textContent = name.replace(/aistrike01\//g, "");
+
     work.append(workLink);
+    work.append(img);
     works.append(work);
 }
 
@@ -29,7 +35,7 @@ async function getRepos() {
 
     for (let i = 0; i < reposList.length; i++) {
         const element = reposList[i];
-        addWork(element.full_name, element.html_url);
+        addWork(element.full_name, element.html_url, element.language);
     }
 }
 
